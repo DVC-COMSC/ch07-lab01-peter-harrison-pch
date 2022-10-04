7110c39c5805881025ac99e95d358d9afe7c6008
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-04 19:23:42 +0000

    save work

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 # ******************************
 # Make your Code
 # ******************************
+numbers = []
+
+for i in range(5):
+    numbers.append(int(input()))
+
+total = sum(numbers)
+total = total - min(numbers) - max(numbers)
